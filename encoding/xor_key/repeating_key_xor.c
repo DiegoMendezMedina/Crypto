21fca62eeb090edb649dc5e9382e5377902c7ad6
diego.mendez.medina <diegomm@ciencias.unam.mx>
2021-10-20 13:37:51 -0500

    key xor terminal

--- encoding/xor_key/repeating_key_xor.c
+++ encoding/xor_key/repeating_key_xor.c
@@ -5,27 +5,51 @@
 #include "../../lib/decode.h"
 
 /**
- * This programs encrypts the file received using 
- * repeating-key XOR with the key received on the standard 
+ * This programs encodes the file received using 
+ * repeating-key XOR with the key, received on the standard 
  * input.
+ * Proper execution: ./exe -p password 
+ * Terminal Version.
  */
 
 #define MAXLENGTH 1000
-
+#define MAXPSWD 100
 char* rep_xor(char*, char *);
 
+/* Reapeating key xor */
 int main(int argc, char* argv[])
 {
-  char in[MAXLENGTH];
-  char key[] = "ICE", *xor, *r;
+  int i, j;
+  char in[MAXLENGTH], *xor, *r;
+  
+  if(argc < 3){
+    printf("error: the password may be entered with the -p flag.");
+    printf("\n \t Ej: ./exe -p password\n");
+    return 0;
+  }
 
-  int j = 0;
+  i = 0;
+  while(argv[i][0] != '-')
+    i++;
+  
+  if(argv[i][1] != 'p'){
+    printf("error: flag not recognized\n");
+    return 0;
+  }
+  if(i == argc-1){
+    printf("error: the password may be entered after the -p flag\n");
+    printf("\n \t Ej: ./exe -p password\n");    
+    return 0;
+  }
+
+  
+  j = 0;
   while((in[j] = getchar()) != EOF)
     j++;
   in[j-1] = '\0';
   j--;
   
-  xor = rep_xor(in, key);
+  xor = rep_xor(in, argv[i+1]);
   r = encode_hex(xor, 2*j);
   print_hex(r, 2*j);
 
@@ -34,6 +58,9 @@ int main(int argc, char* argv[])
   return 0;
 }
 
+/**
+ * rep_xor: Encodes src with xor repeating the key.
+ */
 char* rep_xor(char* src, char* key)
 {
   int i, j, length, sum, ik, l_key;
